12fd6551b16261d4c15f010111d89b45d3f4d7d2
mariakon14 <95983059+mariakon14@users.noreply.github.com>
2022-01-18 20:45:55 +0200

    Update Round.java

--- Round.java
+++ Round.java
@@ -1,7 +1,13 @@
+import java.util.Random;
+
 public class Round {
     private int roundSeconds;
     
     private int category;
+    
+    Random r = new Random();
+    
+    String catName;
 
     private int roundScore;
     
@@ -9,8 +15,26 @@ public class Round {
     public Round(int roundSeconds, int category) {
         this.roundSeconds = roundSeconds;
         this.category = category;
+        setCatName();
     }
 
+    private void setCatName() {
+    	switch (category) {
+    		case 1:
+    			catName = "Μ";
+    			break;
+    		case 2:
+    			catName = "Π";
+    			break;
+    		case 3:
+    			catName = "Γ";
+    			break;
+    		default:
+    			catName = "Ι";
+    			break;
+    	}
+    	catName = String.format("%s%02d", catName, r.nextInt(20) + 1);
+    }
 
     public int getRoundScore() {
         return roundScore;
@@ -18,13 +42,12 @@ public class Round {
 
 
     public void runRound() {
-        //ΕΜΦΑΝΙΣΗ ΕΡΩΤΗΣΗΣ kai apanthshs
-        System.out.println("Πατήστε οποιοδήποτε πλήκτρο για να ξεκινήσει ο γύρος");
+        //database connection εμφάνιση ερωτησης / απάντησης ανάλογα με κατηγορίες
+        System.out.println("Πατήστε enter για να ξεκινήσει ο γύρος");
         Input.getStringInput();
         System.out.println("Έχεις " + roundSeconds + " δευτερόλεπτα");
-        Countdown c = new Countdown(roundSeconds);
+        Countdown c = new Countdown(roundSeconds, this);
         c.run();
-        //c.getroundScore();
-        //team1addscore
+        roundScore = c.roundScore;
     }
 }
